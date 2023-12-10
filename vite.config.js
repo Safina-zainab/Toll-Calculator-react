f57e91d6b13540cabf4246a990c890e7daf91721
safina zainab <116248614+Safina-zainab@users.noreply.github.com>
2023-12-10 20:36:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
      rollupOptions: {
-      external: ['leaflet']
+      external: ['leaflet','leaflet/dist/leaflet.css']
     },
   },
 });

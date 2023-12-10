679202a008cb48a8ff072d8c47fadbe3b39e5d3a
safina zainab <116248614+Safina-zainab@users.noreply.github.com>
2023-12-10 19:56:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteCSSPlugin from 'vite-plugin-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteCSSPlugin],
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios', 'leaflet', '@googlemaps/polyline-codec'],
+      // external: ['axios', 'leaflet', '@googlemaps/polyline-codec'],
     },
   },
 });
